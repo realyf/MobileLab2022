1e60f057dd0a34b6794f59f04ec1d1808f5d0037
realyf <realyuanfang@gmail.com>
2022-02-04 17:25:47 -0500

    In-class exercise

--- Week 2-playground.playground/Pages/Page 1.xcplaygroundpage/Contents.swift
+++ Week 2-playground.playground/Pages/Page 1.xcplaygroundpage/Contents.swift
@@ -2,6 +2,37 @@
 
 import Foundation
 
-var greeting = "Hello, playground"
+let str = "🧨🧧🎉🏮"
+
+func charAt(_ str:String, _ offset:Int) -> String {
+  let index = str.index(str.startIndex, offsetBy: offset)
+  let char = str[index]
+  return String(char)
+}
+
+let randomInt = Int.random(in: 0..<str.count)
+
+func generateLine(_ n:Int) {
+  var nstr = ""
+  for _ in 0..<n {
+    let randomInt = Int.random(in: 0..<str.count)
+    // print(index, "randomInt", randomInt)
+    nstr += charAt(str, randomInt)
+  }
+  print(nstr)
+}
+
+func generateBlock(_ n: Int) {
+  for _ in 0..<n {
+    generateLine(n)
+  }
+}
+
+generateBlock(10)
+print("")
+generateBlock(5)
+print("")
+generateBlock(5)
+
 
 //: [Next](@next)
